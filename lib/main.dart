65488234be6b2975724f6074674a65b4df7b6fd5
Buddhima Fernando <buddhimafernando@Buddhimas-MacBook-Pro.local>
2025-08-11 13:59:20 +0530

    character screen layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:hp_explore_mobile/core/theme/theme.dart';
 import 'package:hp_explore_mobile/presentation/screens/splash_screen.dart';
-
-import 'presentation/screens/landing_screen.dart';
 
 void main() {
   runApp(const MyApp());
