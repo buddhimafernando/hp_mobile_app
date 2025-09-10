894dbc77b106dacc8e5a6c69c791260b8f6eb5c2
Buddhima Fernando <buddhimafernando@Buddhimas-MacBook-Pro.local>
2025-09-10 13:55:01 +0530

    books model, service and provider

---
+++ lib/data/models/books_model/books_model.dart
@@ -0,0 +1,21 @@
+class BooksModel {
+  final int number;
+  final String title;
+  final String originalTitle;
+  final String releaseDate;
+  final String description;
+  final String pages;
+  final String cover;
+  final int index;
+
+  BooksModel({
+    required this.number,
+    required this.title,
+    required this.originalTitle,
+    required this.releaseDate,
+    required this.description,
+    required this.pages,
+    required this.cover,
+    required this.index,
+  });
+}
